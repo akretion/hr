7da449eef039e0431f372851ab7cb28af29f2e88
Florian da Costa <florian.dacosta@akretion.com>
2016-07-26 17:29:35 +0200

    Add hr_company_calendar

---
+++ hr_company_calendar/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2011-2016 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    'name': "Company Calendar",
+    'version': '9.0.1.0.0',
+    'author': "Akretion,Odoo Community Association (OCA)",
+    'maintainer': 'Odoo Community Association (OCA)',
+    'category': 'Human Resources',
+    'complexity': 'easy',
+    'depends': [
+        'resource',
+    ],
+    'website': 'http://www.akretion.com',
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
